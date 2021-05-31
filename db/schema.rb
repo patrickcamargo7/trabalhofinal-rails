dac062cc3b73d795e91d993c796390ffcd4bfc36
patrickcamargo7 <patrickcsilv@gmail.com>
2021-05-31 19:42:47 -0300

    Adicionado links em navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_221838) do
+ActiveRecord::Schema.define(version: 2021_05_31_223620) do
 
   create_table "raffles", force: :cascade do |t|
     t.string "username"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2021_05_31_221838) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_raffles_on_user_id"
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string "description"
+    t.integer "initial_number"
+    t.integer "amount"
+    t.integer "step"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
