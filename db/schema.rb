50697a868f718b4128902c36431c4d64af877b86
patrickcamargo7 <patrickcsilv@gmail.com>
2021-05-31 18:44:22 -0300

    Permitindo adição de nome de usuário no device

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_04_06_022426) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
