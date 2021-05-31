aa2f60a79b79477210c10d42ac4818b9fbb75b36
patrickcamargo7 <patrickcsilv@gmail.com>
2021-05-31 09:55:00 -0300

    Redirecionamento de rotas(device) e definido nova rota de root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  root to: "raffles#index"
+
+
   devise_for :users
   resources :raffles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
